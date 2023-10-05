1d290b58e368e838fc1face4fb9a0f2146851de5
bushra-aekay <bushraabdulkhader527@gmail.com>
2023-10-05 19:37:13 +0530

    made changes to login, register and improved css

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-export const Login = () => {
+
+export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
@@ -11,12 +12,16 @@ export const Login = () => {
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label for="email">email</label>
-            <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label for="password">password</label>
-            <input value={pass} type="password" placeholder="*****" id="password" name="password" />
-            <button type="submit">Log In</button>
-        </form>
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">Password</label>
+                <input value={pass} type="password"onChange={(e) => setPass(e.target.value)} placeholder="*****" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here. </button>
+        </div>
     )
 }
